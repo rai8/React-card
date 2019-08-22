05cb2873b4d0234e9b0f4cc854228946f27895ce
rai8 <hytonne@outlook.com>
2019-08-22 12:45:08 +0300

    final project

--- src/Components/CardsUI.js
+++ src/Components/CardsUI.js
@@ -17,13 +17,13 @@ export default function Cards(props) {
               itching. You get a healthy buzz from the endless possibilities
               that await you – you’re inspired by that one amazing idea. Let’s
               face it – you know you’re going to drop any and all plans and dive
-              into this proje className="image"ct with every drop of energy at
-              your disposal. You’re going to be up all day and all night. Sooner
-              or later, you snap, drop everything, and start coding this
-              exciting new idea into reality – one line at a time. Most of the
-              time, this burst of motivation is a good thing. It catalyzes
-              action like a drop of nitro inside your fuel tank. You feel
-              hyper-focused and complete tasks at lightning speed.{" "}
+              into this project with every drop of energy at your disposal.
+              You’re going to be up all day and all night. Sooner or later, you
+              snap, drop everything, and start coding this exciting new idea
+              into reality – one line at a time. Most of the time, this burst of
+              motivation is a good thing. It catalyzes action like a drop of
+              nitro inside your fuel tank. You feel hyper-focused and complete
+              tasks at lightning speed.{" "}
             </p>
             <button className="btn btn-outline-success">
               Find more about them
